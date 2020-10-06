b2bc82605c31e1e3afd193b76750060744d89207
kater328 <kattyzz328@gmail.com>
2020-10-06 16:08:54 +0300

    feat: implement reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let result = '';
+  if (str[0] === '-') str = str.substring(1);
+  for (let i = str.length - 1; i >= 0; i--)
+      result = result + str[i];
+  return result;
 }
